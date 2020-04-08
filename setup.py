361a79cf59278d059459898f6c6635af6133167c
evitts1989 <evitts1989@gmail.com>
2020-04-07 21:07:24 -0500

    .

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ setup(
     name="my_date_creator", # the name that you will install via pip
     version="1.0",
     author="Corey Evitts",
-    author_email="evitts1989@gmail.com,
+    author_email="evitts1989@gmail.com",
     description="Creates new monthly/yearly columns from a date column",
     long_description=long_description,
     long_description_content_type="text/markdown", # required if using a md file for long desc
     #license="MIT",
-    url="https://github.com/YOUR_USERNAME/YOUR_REPO_NAME",
+    url="https://github.com/evitts1989/my_lambdata_ce",
     #keywords="",
     packages=find_packages() # ["my_lambdata"]
 )
